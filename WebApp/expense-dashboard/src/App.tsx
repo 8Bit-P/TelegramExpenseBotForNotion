1cfc2b30bcb3b31e52f85d9c36554df23fbb4529
Pablo Pérez Martín <aperezvic21@gmail.com>
2025-06-23 16:41:12 +0200

    Basic ReactJS + Vite + Tailwind + Shadcn setup

---
+++ WebApp/expense-dashboard/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+    <h1>
+      HOLA
+    </h1>
+    <Button>hOLA</Button>
+    </>
+  )
+}
+
+export default App
