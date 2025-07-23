8ec8a0e4fbbc4c635646d35beb01a924940b33aa
Pablo Pérez Martín <aperezvic21@gmail.com>
2025-07-23 19:55:18 +0200

    Change to relative imports

--- WebApp/expense-dashboard/src/components/login-page.tsx
+++ WebApp/expense-dashboard/src/components/login-page.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { supabase } from "@/lib/supabase";
+import { supabase } from "../lib/supabase";
 import { LoginForm } from "./login-form";
 
 export default function LoginPage() {
